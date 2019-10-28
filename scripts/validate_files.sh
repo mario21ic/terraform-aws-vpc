239ee6a2d8ebe72a4e6b616f04416468e0428367
Mario Inga <mario21ic@gmail.com>
2019-10-28 01:13:05 -0500

    feat(docker) updating scripts to test

--- scripts/validate_files.sh
+++ scripts/validate_files.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-find . -type f -name "*.tf" -exec dirname {} \;|sort -u | while read m; do (terraform validate -check-variables=false "$m" && echo "√ $m") || exit 1 ; done
+find . -type f -name "*.tf" -exec dirname {} \;|sort -u | while read m; do (terraform validate "$m" && echo "√ $m") || exit 1 ; done
--- scripts/validate_tflint.sh
+++ scripts/validate_tflint.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-docker run --rm -v $(pwd):/data -t wata727/tflint
+#docker run --rm -v $(pwd):/data -t wata727/tflint
+tflint
+if [ $? != 0 ]; then
+  #exit 1
+  echo "Error"
+fi
+
